915fb218b34ebfca5331c004920ff499c22fc1ce
Waiyanheinoo <waiyanheinoo99@gmail.com>
2026-04-27 02:43:47 +0700

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
-import cors from "cors";
 import router from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT;
 if (!PORT) throw new Error("PORT is missing in your env file");
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(router);
